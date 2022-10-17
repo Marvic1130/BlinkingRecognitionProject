0fe74a8506a52690771eac1343e9b9bbaed3038d
DESKTOP-RKMQ2M7\user <wjdtjr9401@naver.com>
2022-10-17 17:30:36 +0900

    #DB연결 오류 수정

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const user = require("../controlers/userControler");
+const user = require("../controllers/userController");
 
 // import express from "express";
 // import { home } from "../controlers/userControler.js";
-const userRouter = express.Router();
+const userRouter = express.Router(); //라우터 생성
 
 userRouter.get("/", user.home);
 userRouter.post("/sJoin", user.studentJoin);
