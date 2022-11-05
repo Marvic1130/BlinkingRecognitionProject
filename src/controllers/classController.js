3b9320a7abeb7c804de98700cf1f65f5f600cb64
DESKTOP-RKMQ2M7\user <wjdtjr9401@naver.com>
2022-11-05 15:11:35 +0900

    #6

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const { Op } = require("sequelize");
 const Class = require("../models/Class");
 
@@ -50,3 +51,52 @@ module.exports.find = (req, res) => {
 };
 
 //class등록했을때 user랑 연결되는지 확인!!!sㄴ
+=======
+const { Op } = require("sequelize")
+const Class = require("../models/Class");
+
+module.exports.register = async(req, res) => {
+    console.log(req.user)
+    const { className, professor, department, classTime, place, people } = req.body;
+  
+    try {
+      await Class.create({
+        className,
+        professor,
+        department,
+        classTime,
+        place,
+        people,
+        userId : req.user.userId
+      });
+      return res.json({userId , className, professor, department, classTime, place, people}).redirect("/")
+    } catch (err) {
+      console.log(err);};
+}
+
+module.exports.find = (req, res) => {
+    //ui 변경 요청하기!!!!!
+    const { keyword } = req.params
+    const item = Class.findAll({
+        where :{
+            [Op.or]: [
+                {
+                    className :{
+                        [Op.like] : `%${keyword}%`
+                    }
+                },
+                {
+                    professor : {
+                        [Op.like] : `%${keyword}%`
+                }
+            }
+            ]
+        }
+    })
+    if(item.length === 0){
+        return res.status(401).json({ message : "일치하는 수업이 없습니다!"})
+    }
+  };
+
+  //class등록했을때 user랑 연결되는지 확인!!!
+>>>>>>> dce9d63 (class 등록, class 검색 기능 구현)
--- src/routers/classRouter.js
+++ src/routers/classRouter.js
@@ -5,6 +5,7 @@ const authToken = require("../middlewares");
 const classRouter = express.Router(); //라우터 생성
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 //미들웨어 설정
 classRouter.route("/register").all(authToken).post(classes.register);
 classRouter.post("/find", classes.find);
@@ -15,4 +16,10 @@ classRouter.post("/find", classes.find);
 
 // export default userRouter;
 >>>>>>> 769b83b (token 유효성검사)
+=======
+//미들웨어 설정
+classRouter.route("/register").all(authToken).post(classes.register)
+classRouter.post("/find", classes.find);
+
+>>>>>>> dce9d63 (class 등록, class 검색 기능 구현)
 module.exports = classRouter;
